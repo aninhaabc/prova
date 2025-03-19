dc89ca1c4f792dfaff5a7da1f25d6c1805a3db87
aninhaabc <aninhabiac123@gmail.com>
2025-03-19 17:04:15 -0300

    add tests

--- src/test/java/br/insper/prova/controller/TesteControllerTests.java
+++ src/test/java/br/insper/prova/controller/TesteControllerTests.java
@@ -20,7 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.Arrays;
 import java.util.List;
 
-
+@ExtendWith(MockitoExtension.class)
 public class TesteControllerTests {
     @InjectMocks
     private TesteController testeController;
